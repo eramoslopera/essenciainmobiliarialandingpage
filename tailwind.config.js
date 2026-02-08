8c772321e15c626804163a94aaf93d1bb8d71316
Esteban Ramos Lopera <destebanrl@MacBook-Pro-de-Esteban-2.local>
2026-02-08 22:43:38 +0100

    Fix tailwind content paths and tsconfig aliases

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 export default {
     content: [
         "./index.html",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: "class",
     theme: {
