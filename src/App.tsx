65b2e4bc8a18801d75283c69cfe1e1eb6bcbc4e3
Esteban Ramos Lopera <destebanrl@MacBook-Pro-de-Esteban-2.local>
2026-02-08 22:38:01 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/landing" element={<Landing />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
